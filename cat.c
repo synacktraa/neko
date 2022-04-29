029ad74c9a3620e40f204a1c7b7c328941219a8d
SynAcktraa <harshmetaexploit90@gmail.com>
2022-04-29 21:57:34 +0530

    strips repeated empty lines and better arg parsing

--- cat.c
+++ cat.c
@@ -47,7 +47,7 @@ static int fexists(const char * file) {
 }
 
 
-static void cat(char*file, int lflag) {
+static void cat(char*file, int lflag, int sflag) {
 
     char *pline = NULL;
     if(lflag)
@@ -60,18 +60,26 @@ static void cat(char*file, int lflag) {
     
     if(buffer == NULL) exit(1);
 
-    int i = 1;
+    int i = 1, s = 0;
+
     while (fgets(buffer, buflen, fptr) != NULL){
+        if(sflag && (buffer[0] == 10 || buffer[0] == 13)){
+            if(s >= 1)
+                continue;
+            s++;
+        }
         fprintf(stdout, pline, 6, i);
         fprintf(stdout, "%s", buffer);
-        ++i; 
-    }
+        if((buffer[0] != 10 && buffer[0] != 13))
+            s = 0;
+        ++i;
+    } 
     free(buffer);
 }
 
 static int arg_validate(char* arg){
-    //will be adding more flags
-    if(!strcmp(arg, "-n")){
+    if(!strcmp(arg, "-n")||
+       !strcmp(arg, "-s")){
             return 1;
         }
     return 0;
@@ -81,30 +89,33 @@ static int arg_validate(char* arg){
 int main(int argc, char**argv) {
 
     char *file = NULL;
-    int lflag = 0;
+    int lflag = 0, sflag = 0;
     if (argc == 2){
         if(!arg_validate(argv[1]))
             file = argv[1];
-    } else if (argc == 3){
+    } else if (argc > 2 || argc <= 4){
         int i;
-        for(i = 1; i < 3; i++){
-            if(!strcmp(argv[i], "-n")){
+        for(i = 1; i < argc; i++){
+            if(!strcmp(argv[i], "-n"))
                 lflag = 1;
-                break;
+            if(!strcmp(argv[i], "-s"))
+                sflag = 1;
+            if(!strcmp(argv[i], "-sn") || !strcmp(argv[i], "-ns")){
+                sflag = 1;
+                lflag = 1;
             }
+            if(!arg_validate(argv[i]))
+                file = argv[i];
         }
-        if(i==1)
-            file = argv[2];
-        else if (i==2)
-            file = argv[1];
+        
     }
     char *exe = basename(argv[0]);
     if(!fexists(file)) {
-        fprintf(stderr, "%s: %s: No such file or directory", exe, file);
+        fprintf(stderr, "\n%s: %s: No such file or directory", exe, file);
         return 1;
     }
     free(exe);
-    cat(file, lflag);
+    cat(file, lflag, sflag);
     return 0;
 
 }
